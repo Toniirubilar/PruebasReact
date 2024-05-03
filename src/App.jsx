1c009a62ff1d0862fa50a9a47ca3a0fd39e73b86
Toniirubilar <laboralagus@gmail.com>
2024-05-02 23:58:13 -0300

    primer comiteo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+
+function App (){
+  return (
+  <>
+  <h1>Hola manu gorriado</h1>
+  </>
+
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+
+
+function Navbar () {
+    return (
+
+        <>
+            <div className="navbar">
+            <a href="https:/www.google.com"><button>Google</button></a>
+            <a href="https:/www.youtube.com"><button>Youtube</button></a>
+            <a href="https:/www.Linkedin.com"><button>Linkedin</button></a>
+            </div>
+        </>
+
+
+)
+}
+
+export default Navbar
